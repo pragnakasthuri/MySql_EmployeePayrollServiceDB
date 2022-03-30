25f81b02687056d5332879f17db1cceed9767a8e
pragnakasthruri <kpragna25@gmail.com>
2022-03-30 19:46:27 +0530

    Adding the test case for updating the salary

--- src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -8,8 +8,17 @@ import java.util.List;
 public class EmployeePayrollServiceTest {
     @Test
     public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
-        EmployeePayrollDBService employeePayrollDBService = new EmployeePayrollDBService();
+        EmployeePayrollDBService employeePayrollDBService = EmployeePayrollDBService.getInstance();
         List<EmployeePayrollData> employeePayrollData= employeePayrollDBService.readData();
         Assertions.assertEquals(9,employeePayrollData.size());
+    }
+
+    @Test
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData();
+        employeePayrollService.updateEmployeeSalary("Terissa", 3000000);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terissa");
+        Assertions.assertTrue(result);
     }
 }
