24cf35d6fa228df482d7bb0eb98ed2a5b3baffc3
pragnakasthruri <kpragna25@gmail.com>
2022-03-31 16:30:12 +0530

    Adding the test case for counting the employees for given date range

--- src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -3,14 +3,22 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class EmployeePayrollServiceTest {
     @Test
     public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
-        EmployeePayrollDBService employeePayrollDBService = EmployeePayrollDBService.getInstance();
-        List<EmployeePayrollData> employeePayrollData= employeePayrollDBService.readEmployeePayrollData();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData();
         Assertions.assertEquals(9,employeePayrollData.size());
+    }
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldNotMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData();
+        Assertions.assertNotEquals(6,employeePayrollData.size());
     }
 
     @Test
@@ -20,5 +28,23 @@ public class EmployeePayrollServiceTest {
         employeePayrollService.updateEmployeeSalary("Terissa", 3000000);
         boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terissa");
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        LocalDate startDate = LocalDate.of(2019,01, 01);
+        LocalDate endDate = LocalDate.now();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollForDateRange(startDate, endDate);
+        Assertions.assertEquals(8, employeePayrollData.size());
+    }
+
+    @Test
+    public void givenDateRange_WhenRetrieved_ShouldNotMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        LocalDate startDate = LocalDate.of(2019,01, 01);
+        LocalDate endDate = LocalDate.now();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollForDateRange(startDate, endDate);
+        Assertions.assertNotEquals(3, employeePayrollData.size());
     }
 }
